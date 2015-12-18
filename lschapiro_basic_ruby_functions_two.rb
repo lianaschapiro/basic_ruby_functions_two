8d8167f8fb5f098aa10ff31a5b61b881f01eb32a
lianaschapiro <liana.schapiro@gmail.com>
2015-12-18 17:55:38 -0500

    Created output sentence that tells user most frequent word. Assignment completed.

--- lschapiro_basic_ruby_functions_two.rb
+++ lschapiro_basic_ruby_functions_two.rb
@@ -2,6 +2,8 @@
 
 # Start by storing the text within a hash where each word is a key with the word's value being the amount of times it has been used in the block:
 # ruby {the: 10, john: 1, of: 15}
+# Once you've created this hash, return the word that has been used the most
+# When you're done, encapsulate your script inside of a method that can analyze any block of text fed to it
 
 def analyze_text (document)
 	document.downcase!
@@ -15,15 +17,13 @@ def analyze_text (document)
 		end
 		i+=1
 	end
-	puts doc_hash 
+	best_word = doc_hash.sort_by{|k, v| v}.last[0]
+	puts "The most frequent word is " + best_word
 end
 
-# Once you've created this hash, return the word that has been used the most
-# When you're done, encapsulate your script inside of a method that can analyze any block of text fed to it
+analyze_text ("You you you are are silly silly silly silly silly silly")
 
 # Bonus assignment: Create a "choose your own adventure" Ruby game using the gets ruby method to get user input from the command line. Have the game send the user down many different paths depending on the input that they enter. Use objects to store data about the user and different items they could collect along the way. Seehttp://en.wikipedia.org/wiki/Interactive_fiction (Links to an external site.) for a more in-depth description of "choose your own adventure"-style command line games.
-
-# Note: If you complete assignment 1 only, you are eligible for a 10/10 for homework. The bonus assignment is just a great way to get a ton more Ruby practice!
 
 # Recommended Activity
 # Complete as much of the "Introduction to Ruby" track on Codecademy as you can. http://www.codecademy.com/courses/ruby-beginner-en-d1Ylq?curriculum_id=5059f8619189a5000201fbcb) (Links to an external site.)
